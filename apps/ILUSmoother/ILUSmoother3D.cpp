4421fd3a8d07611e406b5c47de105eeb1d1257ee
wagnandr <wagnandr@in.tum.de>
2022-04-24 17:39:12 +0200

    adds estimator for ldlt smoother symbol

--- apps/ILUSmoother/ILUSmoother3D.cpp
+++ apps/ILUSmoother/ILUSmoother3D.cpp
@@ -158,6 +158,255 @@ std::shared_ptr< hyteg::Solver< OperatorType > >
    WALBERLA_ABORT( "smoother type " + smoother_type + " is unknown." );
 }
 
+namespace hyteg {
+namespace ldlt {
+namespace p1 {
+namespace dim3 {
+
+std::map< SD, real_t > calculateAsymptoticLDLTStencil( std::map< SD, real_t >& a_stencil )
+{
+   std::map< stencilDirection, real_t > l_stencil_prev;
+   std::map< stencilDirection, real_t > l_stencil_next;
+   for ( auto d : hyteg::ldlt::p1::dim3::allDirections )
+   {
+      l_stencil_prev[d] = 0;
+      l_stencil_next[d] = 0;
+   }
+   l_stencil_prev[SD::VERTEX_C] = 1.;
+   l_stencil_next[SD::VERTEX_C] = 1.;
+
+   // asymptotic iteration:
+   for ( uint_t i = 0; i < 100; i += 1 )
+   {
+      const real_t a_bc  = a_stencil[SD::VERTEX_BC];
+      const real_t a_s   = a_stencil[SD::VERTEX_S];
+      const real_t a_bnw = a_stencil[SD::VERTEX_BNW];
+      const real_t a_be  = a_stencil[SD::VERTEX_BE];
+      const real_t a_w   = a_stencil[SD::VERTEX_W];
+      const real_t a_bn  = a_stencil[SD::VERTEX_BN];
+      const real_t a_se  = a_stencil[SD::VERTEX_SE];
+      const real_t a_c   = a_stencil[SD::VERTEX_C];
+
+      // beta_bc:
+      real_t beta_bc = a_bc / l_stencil_prev[SD::VERTEX_C];
+      // beta_s:
+      real_t beta_s = a_s;
+      beta_s -= beta_bc * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_BN];
+      beta_s /= l_stencil_prev[SD::VERTEX_C];
+      // beta_bnw:
+      real_t beta_bnw = a_bnw;
+      beta_bnw -= beta_bc * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_SE];
+      beta_bnw /= l_stencil_prev[SD::VERTEX_C];
+      // beta_be:
+      real_t beta_be = a_be;
+      beta_be -= beta_bc * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_W];
+      beta_be /= l_stencil_prev[SD::VERTEX_C];
+      // beta_w:
+      real_t beta_w = a_w;
+      beta_w -= beta_bc * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_BE];
+      beta_w -= beta_bnw * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_BN];
+      beta_w -= beta_s * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_SE];
+      beta_w /= l_stencil_prev[SD::VERTEX_C];
+      // beta_bn:
+      real_t beta_bn = a_bn;
+      beta_bn -= beta_bc * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_S];
+      beta_bn -= beta_be * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_SE];
+      beta_bn -= beta_bnw * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_W];
+      beta_bn /= l_stencil_prev[SD::VERTEX_C];
+      // beta_se:
+      real_t beta_se = a_se;
+      beta_se -= beta_bc * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_BNW];
+      beta_se -= beta_be * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_BN];
+      beta_se -= beta_s * l_stencil_prev[SD::VERTEX_C] * l_stencil_prev[SD::VERTEX_W];
+      beta_se /= l_stencil_prev[SD::VERTEX_C];
+      // beta_c:
+      real_t beta_c = a_c;
+      beta_c -= beta_bc * beta_bc * l_stencil_prev[SD::VERTEX_C];
+      beta_c -= beta_be * beta_be * l_stencil_prev[SD::VERTEX_C];
+      beta_c -= beta_bnw * beta_bnw * l_stencil_prev[SD::VERTEX_C];
+      beta_c -= beta_bn * beta_bn * l_stencil_prev[SD::VERTEX_C];
+      beta_c -= beta_se * beta_se * l_stencil_prev[SD::VERTEX_C];
+      beta_c -= beta_s * beta_s * l_stencil_prev[SD::VERTEX_C];
+      beta_c -= beta_w * beta_w * l_stencil_prev[SD::VERTEX_C];
+
+      l_stencil_next[SD::VERTEX_BC]  = beta_bc;
+      l_stencil_next[SD::VERTEX_S]   = beta_s;
+      l_stencil_next[SD::VERTEX_BNW] = beta_bnw;
+      l_stencil_next[SD::VERTEX_BE]  = beta_be;
+      l_stencil_next[SD::VERTEX_W]   = beta_w;
+      l_stencil_next[SD::VERTEX_BN]  = beta_bn;
+      l_stencil_next[SD::VERTEX_SE]  = beta_se;
+      l_stencil_next[SD::VERTEX_C]   = beta_c;
+
+      real_t diff = 0;
+      for ( auto d : hyteg::ldlt::p1::dim3::lowerDirectionsAndCenter )
+      {
+         diff += std::abs( l_stencil_prev[d] - l_stencil_next[d] );
+      }
+      WALBERLA_LOG_INFO_ON_ROOT( "difference in iteration " << i << " is " << diff );
+
+      l_stencil_prev = l_stencil_next;
+
+      if ( diff < 1e-15 )
+      {
+         return l_stencil_prev;
+      }
+   }
+   WALBERLA_ABORT( "could not find asymptotic stencil" );
+}
+
+std::complex< real_t >
+    calculateSymbol( std::map< SD, real_t >& stencil, const std::array< real_t, 3 >& theta, const std::array< real_t, 3 >& h )
+{
+   using namespace std::complex_literals;
+   std::complex< real_t > z_a = 0;
+   for ( auto d : ldlt::p1::dim3::allDirections )
+   {
+      auto k = ldlt::p1::dim3::toIndex( d );
+      z_a += std::exp( ( k[0] * theta[0] * h[0] + k[1] * theta[1] * h[1] + k[2] * theta[2] * h[2] ) * 1i ) * stencil[d];
+   }
+   return z_a;
+}
+
+std::map< SD, real_t > prepare_l_stencil( std::map< SD, real_t >& asymptotic_stencil )
+{
+   using namespace std::complex_literals;
+   std::map< stencilDirection, real_t > l_stencil;
+   for ( auto d : hyteg::ldlt::p1::dim3::allDirections )
+      l_stencil[d] = 0;
+   for ( auto d : hyteg::ldlt::p1::dim3::lowerDirections )
+      l_stencil[d] = asymptotic_stencil[d];
+   l_stencil[SD::VERTEX_C] = 1.;
+   return l_stencil;
+}
+
+std::map< SD, real_t > prepare_lt_stencil( std::map< SD, real_t >& asymptotic_stencil )
+{
+   std::map< stencilDirection, real_t > lt_stencil;
+   for ( auto d : hyteg::ldlt::p1::dim3::allDirections )
+      lt_stencil[d] = 0;
+   for ( auto d : hyteg::ldlt::p1::dim3::lowerDirections )
+      lt_stencil[ldlt::p1::dim3::opposite( d )] = asymptotic_stencil[d];
+   lt_stencil[SD::VERTEX_C] = 1.;
+   return lt_stencil;
+}
+
+std::map< SD, real_t > prepare_d_stencil( std::map< SD, real_t >& asymptotic_stencil )
+{
+   std::map< stencilDirection, real_t > d_stencil;
+   for ( auto d : hyteg::ldlt::p1::dim3::allDirections )
+      d_stencil[d] = 0;
+   d_stencil[SD::VERTEX_C] = asymptotic_stencil[SD::VERTEX_C];
+   return d_stencil;
+}
+
+std::array< real_t, 3 > getMicroEdgeWidths( const Cell& cell, uint_t level )
+{
+   auto                    N  = real_c( levelinfo::num_microedges_per_edge( level ) );
+   real_t                  h1 = ( cell.getCoordinates()[1] - cell.getCoordinates()[0] ).norm() / N;
+   real_t                  h2 = ( cell.getCoordinates()[2] - cell.getCoordinates()[0] ).norm() / N;
+   real_t                  h3 = ( cell.getCoordinates()[3] - cell.getCoordinates()[0] ).norm() / N;
+   std::array< real_t, 3 > h  = { h1, h2, h3 };
+   return h;
+}
+
+template < typename FormType >
+real_t estimateAsymptoticSmootherRate( const Cell& cell, uint_t level, FormType form )
+{
+   form.setGeometryMap( cell.getGeometryMap() );
+   auto a_stencil            = P1Elements::P1Elements3D::calculateStencilInMacroCellForm_new( { 2, 2, 2 }, cell, level, form );
+   auto l_stencil_asymptotic = ldlt::p1::dim3::calculateAsymptoticLDLTStencil( a_stencil );
+
+   const auto h = ldlt::p1::dim3::getMicroEdgeWidths( cell, level );
+
+   // calculate symbols
+   auto l_stencil  = ldlt::p1::dim3::prepare_l_stencil( l_stencil_asymptotic );
+   auto lt_stencil = ldlt::p1::dim3::prepare_lt_stencil( l_stencil_asymptotic );
+   auto d_stencil  = ldlt::p1::dim3::prepare_d_stencil( l_stencil_asymptotic );
+
+   /*
+      WALBERLA_LOG_INFO_ON_ROOT( "a stencil:" );
+      for ( auto d : ldlt::p1::dim3::allDirections )
+         WALBERLA_LOG_INFO( " " << stencilDirectionToStr[d] << " " << a_stencil[d] );
+      WALBERLA_LOG_INFO_ON_ROOT( "l stencil:" );
+      for ( auto d : ldlt::p1::dim3::allDirections )
+         WALBERLA_LOG_INFO( " " << stencilDirectionToStr[d] << " " << l_stencil[d] );
+      WALBERLA_LOG_INFO_ON_ROOT( "d stencil:" );
+      for ( auto d : ldlt::p1::dim3::allDirections )
+         WALBERLA_LOG_INFO( " " << stencilDirectionToStr[d] << " " << d_stencil[d] );
+      WALBERLA_LOG_INFO_ON_ROOT( "lt stencil:" );
+      for ( auto d : ldlt::p1::dim3::allDirections )
+         WALBERLA_LOG_INFO( " " << stencilDirectionToStr[d] << " " << lt_stencil[d] );
+      */
+
+   int    num_samples_half    = 4;
+   int    num_samples         = 2 * num_samples_half;
+   real_t max_symbol          = 0;
+   real_t mean_symbol         = 0;
+   int    total_sample_number = 0;
+   for ( int ix = -( num_samples - 1 ); ix < num_samples; ix += 1 )
+   {
+      for ( int iy = -( num_samples - 1 ); iy < num_samples; iy += 1 )
+      {
+         for ( int iz = -( num_samples - 1 ); iz < num_samples; iz += 1 )
+         {
+            real_t sx = ( real_c( ix ) / num_samples );
+            real_t sy = ( real_c( iy ) / num_samples );
+            real_t sz = ( real_c( iz ) / num_samples );
+
+            real_t                  thetax = pi * sx / h[0];
+            real_t                  thetay = pi * sy / h[1];
+            real_t                  thetaz = pi * sz / h[2];
+            std::array< real_t, 3 > theta  = { thetax, thetay, thetaz };
+
+            bool ix_smooth = ( -pi / 2. / h[0] <= thetax ) && ( thetax <= pi / 2. / h[0] );
+            bool iy_smooth = ( -pi / 2. / h[1] <= thetay ) && ( thetay <= pi / 2. / h[1] );
+            bool iz_smooth = ( -pi / 2. / h[2] <= thetaz ) && ( thetaz <= pi / 2. / h[2] );
+
+            if ( ix_smooth && iy_smooth && iz_smooth )
+               continue;
+
+            auto symbol_a    = ldlt::p1::dim3::calculateSymbol( a_stencil, theta, h );
+            auto symbol_l    = ldlt::p1::dim3::calculateSymbol( l_stencil, theta, h );
+            auto symbol_lt   = ldlt::p1::dim3::calculateSymbol( lt_stencil, theta, h );
+            auto symbol_d    = ldlt::p1::dim3::calculateSymbol( d_stencil, theta, h );
+            auto symbol_ldlt = symbol_l * symbol_d * symbol_lt;
+
+            auto symbol = std::abs( ( symbol_ldlt - symbol_a ) / symbol_ldlt );
+
+            max_symbol = std::max( symbol, max_symbol );
+            mean_symbol += symbol;
+            total_sample_number += 1;
+
+            /*
+               WALBERLA_LOG_INFO_ON_ROOT( sx << " " << sy << " " << sz );
+               WALBERLA_LOG_INFO_ON_ROOT( ix << " " << iy << " " << iz << ", " << theta[0] * h[0] << " " << theta[1] * h[1] << " "
+                                             << theta[2] * h[2] << ", " << symbol << " " << max_symbol )
+               WALBERLA_LOG_INFO_ON_ROOT( "a stencil symbol " << symbol_a );
+               WALBERLA_LOG_INFO_ON_ROOT( "l stencil symbol " << symbol_l );
+               WALBERLA_LOG_INFO_ON_ROOT( "lt stencil symbol " << symbol_lt );
+               WALBERLA_LOG_INFO_ON_ROOT( "d stencil symbol " << symbol_d );
+               WALBERLA_LOG_INFO_ON_ROOT( "ldt stencil symbol " << symbol_ldlt );
+               WALBERLA_LOG_INFO_ON_ROOT( "symbol " << symbol );
+               WALBERLA_LOG_INFO_ON_ROOT( "----------" )
+               */
+         }
+      }
+   }
+
+   /*
+      WALBERLA_LOG_INFO_ON_ROOT( "symbol max " << max_symbol );
+      WALBERLA_LOG_INFO_ON_ROOT( "symbol mean " << mean_symbol / total_sample_number );
+      */
+
+   return max_symbol;
+}
+
+} // namespace dim3
+} // namespace p1
+} // namespace ldlt
+} // namespace hyteg
+
 int main( int argc, char** argv )
 {
    walberla::Environment env( argc, argv );
@@ -401,6 +650,18 @@ int main( int argc, char** argv )
    //using FormType = forms::p1_div_k_grad_affine_q3;
 
    FormType form( kappa3d, kappa2d );
+   FormType form_const( []( auto ) { return 1.; }, []( auto ) { return 1.; } );
+
+   for ( auto cit : storage->getCells() )
+   {
+      Cell& cell = *cit.second;
+
+      using SD = stencilDirection;
+
+      // calculate asymptotic stencil
+      auto max_symbol = ldlt::p1::dim3::estimateAsymptoticSmootherRate( cell, maxLevel, form_const );
+      WALBERLA_LOG_INFO_ON_ROOT( "symbol max " << max_symbol );
+   }
 
    const uint_t opDegreeX     = parameters.getParameter< uint_t >( "op_surrogate_degree_x" );
    const uint_t opDegreeY     = parameters.getParameter< uint_t >( "op_surrogate_degree_y" );
